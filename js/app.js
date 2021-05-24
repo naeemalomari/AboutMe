5f35cf1fc169beecb34e4312170f142c0f57f19d
naeemalomari <naeemalomari96@gmail.com>
2021-05-24 16:17:31 +0300

    AboutMe

--- js/app.js
+++ js/app.js
@@ -12,4 +12,16 @@ alert('I was born in Abudahbi');
 } else {
 alert (' Please Next trial enter yes or no only');
 }
-let
+let naimAge=prompt('Do I look twenty five years old ?');
+naimAge=naimAge.toLocaleLowerCase();
+switch(naimAge){
+case'yes':
+alert('yes I am 25');
+break;
+case 'no':
+alert('do not increase or decrease my age. I am 25 years old');
+break;
+default:
+alert('your answer should be yes or no only');
+break;
+}
