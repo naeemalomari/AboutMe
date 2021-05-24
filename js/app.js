5614c4364bbc39856f9e77b14d3199c0a37cc9d3
naeemalomari <naeemalomari96@gmail.com>
2021-05-24 16:32:53 +0300

    AboutMe

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-let UserName=prompt('whats your name?');
+let UserName=prompt('Hello, whats your name?');
 console.log(UserName);
 let PlaceofBirth=prompt('Do you think Naim was born in Jordan?');
 console.log(PlaceofBirth);
@@ -12,16 +12,28 @@ alert('I was born in Abudahbi');
 } else {
 alert (' Please Next trial enter yes or no only');
 }
+
 let naimAge=prompt('Do I look twenty five years old ?');
+console.log(naimAge);
 naimAge=naimAge.toLocaleLowerCase();
 switch(naimAge){
 case'yes':
 alert('yes I am 25');
 break;
 case 'no':
-alert('do not increase or decrease my age. I am 25 years old');
+alert('Do not increase or decrease my age. I am 25 years old');
 break;
 default:
-alert('your answer should be yes or no only');
+alert('Your answer should be yes or no only');
 break;
+}
+let myCar=prompt('Do you like cars ?');
+console.log(myCar);
+myCar=myCar.toLocaleLowerCase();
+if(myCar==='yes' || myCar==='y'){
+alert('I have BMW I really love it ');
+} else if (myCar==='no'||myCar==='n'){
+alert('Bad to hear that Welcome to my site anyway');
+} else {
+alert('Your answer should be yes or no');
 }
